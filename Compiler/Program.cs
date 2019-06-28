37d083af16fb93e02d6c925b5ff766d56778dbf8
Shane Celis <shane.celis@gmail.com>
2019-06-27 22:11:24 -0400

    feature: Make compiler write netstandard2.0 DLL.

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -12,34 +12,52 @@ namespace Compiler
     {
         public static void Main(string[] args)
         {
+
+          var netstandardDllDirectory = "/Users/shane/Downloads/netstandard.library.2.0.3/build/netstandard2.0/ref";
+          var netstandardReferences =
+            Directory.GetFiles(netstandardDllDirectory, "*.dll")
+            .Select(file => MetadataReference.CreateFromFile(file))
+            .ToList();
             var metadataReferences = new[]
             {
-                typeof(object).GetTypeInfo().Assembly, // System.Private.CoreLib.dll
-                typeof(Enumerable).GetTypeInfo().Assembly, // System.Linq.dll
-                typeof(Console).GetTypeInfo().Assembly, // System.Console.dll
-                Assembly.Load(new AssemblyName("System.Runtime")), // System.Runtime.dll
-                Assembly.Load(new AssemblyName("Calculator")) // Calculator.dll
-            }.Select(x => MetadataReference.CreateFromFile(x.Location)).ToList();
+                typeof(System.Reactive.Unit).GetTypeInfo().Assembly, // System.Private.CoreLib.dll
+                // typeof(Enumerable).GetTypeInfo().Assembly, // System.Linq.dll
+                // typeof(Console).GetTypeInfo().Assembly, // System.Console.dll
+                // Assembly.Load(new AssemblyName("System.Runtime")), // System.Runtime.dll
+                // Assembly.Load(new AssemblyName("Calculator")) // Calculator.dll
+            }.Select(x => MetadataReference.CreateFromFile(x.Location))
+                .Concat(netstandardReferences)
+                .ToList();
 
-            var compilationOptions = new CSharpCompilationOptions(OutputKind.ConsoleApplication).
+          var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary).
                 WithMetadataImportOptions(MetadataImportOptions.All);
 
             var topLevelBinderFlagsProperty = typeof(CSharpCompilationOptions).GetProperty("TopLevelBinderFlags", BindingFlags.Instance | BindingFlags.NonPublic);
             topLevelBinderFlagsProperty.SetValue(compilationOptions, (uint)1 << 22);
 
-            var code = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "CrazyProgram", "Program.cs"));
-            var compilation = CSharpCompilation.Create("DynamicCrazyProgram", new[] {
+            // var code = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "..", "CrazyProgram", "Program.cs"));
+            var code = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "..", "BehindTheVeil", "Class1.cs"));
+            // var compilation = CSharpCompilation.Create("DynamicCrazyProgram", new[] {
+            var compilation = CSharpCompilation.Create("BehindTheVeil", new[] {
                 CSharpSyntaxTree.ParseText(code) }, metadataReferences, compilationOptions);
 
-            using (var ms = new MemoryStream())
-            {
-                var cr = compilation.Emit(ms);
-                ms.Seek(0, SeekOrigin.Begin);
-                var assembly = Assembly.Load(ms.ToArray());
-                assembly.EntryPoint.Invoke(null, new object[] { new string[0] });
-            }
+            // using (var ms = new MemoryStream())
+            // {
+                var cr = compilation.Emit("behind-the-veil.dll");
+                //If our compilation failed, we can discover exactly why.
+                if(!cr.Success)
+                {
+                  foreach(var diagnostic in cr.Diagnostics)
+                  {
+                    Console.WriteLine(diagnostic.ToString());
+                  }
+                }
+            //     ms.Seek(0, SeekOrigin.Begin);
+            //     var assembly = Assembly.Load(ms.ToArray());
+            //     assembly.EntryPoint.Invoke(null, new object[] { new string[0] });
+            // }
 
-            Console.ReadKey();
+            // Console.ReadKey();
         }
     }
 }
